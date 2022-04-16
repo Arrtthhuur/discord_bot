d991205a9851a668a82e5d6b957f16d16e218885
abricot <arthurbeznik2@gmail.com>
2022-04-16 21:19:24 +0200

    minor edits

--- bot.py
+++ bot.py
@@ -576,11 +576,10 @@ async def help(ctx, args=None):
         h_embed.set_author(name="=============   Commandes Disponibles   =============")
         h_embed.add_field(name="---------------------------------   Misc   ---------------------------------",
                           value="`!...`", inline=False)
-        h_embed.add_field(name="`!help <commande> / !h <commande>`", value="Plus d'info sur une commande", inline=True)
-        h_embed.add_field(name="`!hello`", value="Couscous", inline=True)
-        
-        h_embed.add_field(name="`!refresh`", value="Refresh le compteur d'humains", inline=True)
-        h_embed.add_field(name="`!coffee`", value="Petit cafe?", inline=True)
+        h_embed.add_field(name="`!help <commande> / !h <commande>`", value="Plus d'info sur une commande", inline=False)
+        h_embed.add_field(name="`!hello`", value="Couscous", inline=False)
+        h_embed.add_field(name="`!refresh`", value="Refresh le compteur d'humains", inline=False)
+        h_embed.add_field(name="`!coffee`", value="Petit cafe?", inline=False)
 
         h_embed.add_field(name="---------------------------------   Move   ---------------------------------",
                           value="`!...`", inline=False)
